dc223cfef0d4a4bef99c585c81278b0b8072ddbe
João Martins <jvmartinsdev@gmail.com>
2023-03-26 01:25:25 -0300

    refactor: change panel api url to /trpc

--- src/pages/api/panel/index.ts
+++ src/pages/api/panel/index.ts
@@ -7,7 +7,7 @@ import { env } from "@/env.mjs";
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
   res.status(200).send(
     renderTrpcPanel(appRouter, {
-      url: `${env.AUTH0_BASE_URL}/api/panel`,
+      url: `${env.AUTH0_BASE_URL}/api/trpc`,
       transformer: "superjson",
     })
   );
